cc8e147722902b4d2ec2f8e2d5e11869e79d4bed
ArthurJacquin <artuj38@free.fr>
2020-09-11 17:20:05 +0200

    preconditionName class added

---
+++ GOAP/GOAP/PreconditionName.h
@@ -0,0 +1,14 @@
+#pragma once
+
+enum class PreconditionName {
+	GetPancake,
+	MixedEggs,
+	MixedFlour,
+	MixedNut,
+	CollectEgg,
+	HaveChicken,
+	CollectFlour,
+	CollectWheat,
+	HaveField,
+	CollectNut
+};
